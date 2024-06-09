086b19439302c59d165636938cb464c1836417f5
Manwe <mathieu.jourdan.zola@gmail.com>
2024-06-09 23:03:59 +0200

    Create a "big_tree" scene with a new sprite to replace some of the invert level 1 trees

--- Scripts/level1.gd
+++ Scripts/level1.gd
@@ -20,8 +20,6 @@ var config_file = config.load("res://Ressources/PropertieFile/properties.cfg")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Tree4.get_child(0).flip_h = true
-	$Tree5.get_child(0).flip_h = true
 	queue.is_level_1 = true
 	queue.is_level_2 = false
 	queue.is_level_3 = false
