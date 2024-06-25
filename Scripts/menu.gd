556765f7139abc4e207b67cfafb4aec11b2aea24
Manwe <mathieu.jourdan.zola@gmail.com>
2024-06-25 15:51:53 +0200

    Change project displaying to fullscreen, and a quit button in menu with new imputs for it,

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -30,6 +30,8 @@ func _process(_delta):
 		else:
 			is_controller_focused = false
 	wait_for_focus()
+	if Input.is_action_just_pressed("quit_game"):
+		get_tree().quit()
 		
 func wait_for_focus():
 	for member in get_tree().get_nodes_in_group("MenuButtons"):
@@ -92,3 +94,15 @@ func _on_level_4_button_gui_input(event):
 		elif event.is_action_pressed("ui_down") or event.is_action_pressed("ui_right"):
 			accept_event() # prevent the normal focus-stuff from happening
 			$Level1Button.grab_focus()
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+func _on_quit_button_focus_entered():
+	$QuitButton/QuitLabel.add_theme_color_override("font_color", Color("#FF0000"))
+
+
+func _on_quit_button_focus_exited():
+	$QuitButton/QuitLabel.add_theme_color_override("font_color", Color("#FFFFFF"))
