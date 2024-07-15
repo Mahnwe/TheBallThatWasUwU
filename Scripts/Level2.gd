df1d92c2dcb4587147530b7e210d2b9130fe71b6
Manwe <mathieu.jourdan.zola@gmail.com>
2024-07-15 17:01:56 +0200

    Add a gameArea in level 2 and add gameArea func in level2 script

--- Scripts/Level2.gd
+++ Scripts/Level2.gd
@@ -208,3 +208,15 @@ func _on_finish_player_entered():
 	print($Player.position.y)
 	$Player.get_child(1).animation = "stay"
 	$Player.set_physics_process(false)
+
+
+func _on_game_area_player_exited_game_area():
+	$Player.set_physics_process(false)
+	$Player.get_child(1).animation = "dead"
+	$Player.get_child(5).text = "Out of game zone !"
+	$Player.get_child(0).get_child(0).get_child(0).get_child(0).set_process(false)
+	await get_tree().create_timer(0.5).timeout;
+	if save_position_x == start_position_x:
+		call_deferred("restart_scene")
+	else:
+		put_player_to_save_position_and_unpause_timer()
