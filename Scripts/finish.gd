817d9116e281804da4ac82e7b2be0e9d94575578
Manwe <mathieu.jourdan.zola@gmail.com>
2024-10-16 03:13:14 +0200

    Separate Finish UI from Finish node, still bug with button hovering

--- Scripts/finish.gd
+++ Scripts/finish.gd
@@ -1,14 +1,10 @@
 extends StaticBody2D
 
 signal player_entered
-signal next_level_pressed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$WellDoneLabel.hide()
-	$FinishUI.hide()
-	$FinishUI.get_child(0).hide()
-	$FinishUI.get_child(1).hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,10 +16,3 @@ func _on_area_2d_body_entered(body):
 	if body.name == "Player":
 		player_entered.emit()
 		$WellDoneLabel.show()
-		$FinishUI.is_UI_showing = true
-		$FinishUI.show()
-		$FinishUI.get_child(0).show()
-		$FinishUI.get_child(1).show()
-
-func _on_finish_ui_next_level_pressed():
-	next_level_pressed.emit()
--- Scripts/finish_ui.gd
+++ Scripts/finish_ui.gd
@@ -7,24 +7,27 @@ var is_controller_focused
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	is_controller_focused = false
-
+	is_UI_showing = false
+	$Blur.hide()
+	$NextLayer.hide()
+	$MenuLayer.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if is_UI_showing:
+		wait_for_focus()
 		if !is_controller_focused and Input.is_action_pressed("ui_down"):
 			print("finish b")
-			$FinishNextLevelButton.grab_focus()
+			$NextLayer/FinishNextLevelButton.grab_focus()
 		elif !is_controller_focused and Input.is_action_pressed("ui_right"):
 			print("finish b")
-			$FinishNextLevelButton.grab_focus()
+			$NextLayer/FinishNextLevelButton.grab_focus()
 		elif !is_controller_focused and Input.is_action_pressed("ui_up"):
 			print("finish m")
-			$FinishMenuButton.grab_focus()
+			$MenuLayer/FinishMenuButton.grab_focus()
 		elif !is_controller_focused and Input.is_action_pressed("ui_left"):
 			print("finish m")
-			$FinishMenuButton.grab_focus()
-		wait_for_focus()
+			$MenuLayer/FinishMenuButton.grab_focus()
 		
 		
 func wait_for_focus():
@@ -39,12 +42,13 @@ func _on_next_level_pressed():
 
 
 func _on_button_focus_entered():
-	is_controller_focused = true
-	$ButtonSound.play()
+	if self.visible == true:
+		is_controller_focused = true
+		$ButtonSound.play()
 
 
 func _on_next_level_mouse_exited():
-	$FinishNextLevelButton.release_focus()
+	$NextLayer/FinishNextLevelButton.release_focus()
 	is_controller_focused = false
 
 
@@ -53,57 +57,43 @@ func _on_menu_pressed():
 
 
 func _on_menu_mouse_exited():
-	$FinishMenuButton.release_focus()
+	$MenuLayer/FinishMenuButton.release_focus()
 	is_controller_focused = false
 
 
 func _on_finish_next_level_button_gui_input(event):
-	if $FinishNextLevelButton.has_focus() and event is InputEventKey or event is InputEventJoypadButton:
+	if $NextLayer/FinishNextLevelButton.has_focus() and event is InputEventKey or event is InputEventJoypadButton:
 		is_controller_focused = true
 		if event.is_action_pressed("ui_left"):
 			accept_event() # prevent the normal focus-stuff from happening
-			print("finish")
-			$FinishMenuButton.grab_focus()
+			$MenuLayer/FinishMenuButton.grab_focus()
 		elif event.is_action_pressed("ui_up"):
 			accept_event()
-			print("finish")
-			$FinishMenuButton.grab_focus()
+			$MenuLayer/FinishMenuButton.grab_focus()
 		elif event.is_action_pressed("ui_down") or event.is_action_pressed("ui_right"):
 			accept_event() # prevent the normal focus-stuff from happening
-			print("finish")
-			$FinishMenuButton.grab_focus()
+			$MenuLayer/FinishMenuButton.grab_focus()
 
 
 func _on_finish_menu_button_gui_input(event):
 	is_controller_focused = true
-	if $FinishMenuButton.has_focus() and event is InputEventKey or event is InputEventJoypadButton:
-		print("menu")
+	if $MenuLayer/FinishMenuButton.has_focus() and event is InputEventKey or event is InputEventJoypadButton:
 		if event.is_action_pressed("ui_left"):
 			accept_event() # prevent the normal focus-stuff from happening
-			$FinishNextLevelButton.grab_focus()
+			$NextLayer/FinishNextLevelButton.grab_focus()
 		elif event.is_action_pressed("ui_up"):
 			accept_event()
-			$FinishNextLevelButton.grab_focus()
+			$NextLayer/FinishNextLevelButton.grab_focus()
 		elif event.is_action_pressed("ui_down") or event.is_action_pressed("ui_right"):
 			accept_event() # prevent the normal focus-stuff from happening
-			$FinishNextLevelButton.grab_focus() 
+			$NextLayer/FinishNextLevelButton.grab_focus() 
 
 
 func _on_finish_next_level_button_focus_exited():
-	$FinishNextLevelButton.release_focus()
+	$NextLayer/FinishNextLevelButton.release_focus()
 	is_controller_focused = false
 
 
 func _on_finish_menu_button_focus_exited():
-	$FinishMenuButton.release_focus()
-	is_controller_focused = false
-
-
-func _on_finish_menu_button_mouse_entered():
-	$FinishMenuButton.grab_focus()
-	is_controller_focused = false
-
-
-func _on_finish_next_level_button_mouse_entered():
-	$FinishNextLevelButton.grab_focus()
+	$MenuLayer/FinishMenuButton.release_focus()
 	is_controller_focused = false
