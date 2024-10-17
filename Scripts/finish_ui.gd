9020413dca8453b5817b9b5f704a1430a326a09d
Manwe <mathieu.jourdan.zola@gmail.com>
2024-10-18 01:51:35 +0200

    Work on finish UI

--- Scripts/finish_ui.gd
+++ Scripts/finish_ui.gd
@@ -9,25 +9,21 @@ func _ready():
 	is_controller_focused = false
 	is_UI_showing = false
 	$Blur.hide()
-	$NextLayer.hide()
-	$MenuLayer.hide()
+	$FinishNextLevelButton.hide()
+	$FinishMenuButton.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if is_UI_showing:
-		wait_for_focus()
 		if !is_controller_focused and Input.is_action_pressed("ui_down"):
-			print("finish b")
-			$NextLayer/FinishNextLevelButton.grab_focus()
+			$FinishNextLevelButton.grab_focus()
 		elif !is_controller_focused and Input.is_action_pressed("ui_right"):
-			print("finish b")
-			$NextLayer/FinishNextLevelButton.grab_focus()
+			$FinishNextLevelButton.grab_focus()
 		elif !is_controller_focused and Input.is_action_pressed("ui_up"):
-			print("finish m")
-			$MenuLayer/FinishMenuButton.grab_focus()
+			$FinishMenuButton.grab_focus()
 		elif !is_controller_focused and Input.is_action_pressed("ui_left"):
-			print("finish m")
-			$MenuLayer/FinishMenuButton.grab_focus()
+			$FinishMenuButton.grab_focus()
+		wait_for_focus()
 		
 		
 func wait_for_focus():
@@ -42,13 +38,12 @@ func _on_next_level_pressed():
 
 
 func _on_button_focus_entered():
-	if self.visible == true:
-		is_controller_focused = true
+	if $ButtonSound.is_playing == false:
 		$ButtonSound.play()
 
 
 func _on_next_level_mouse_exited():
-	$NextLayer/FinishNextLevelButton.release_focus()
+	$FinishNextLevelButton.release_focus()
 	is_controller_focused = false
 
 
@@ -57,43 +52,33 @@ func _on_menu_pressed():
 
 
 func _on_menu_mouse_exited():
-	$MenuLayer/FinishMenuButton.release_focus()
+	$FinishMenuButton.release_focus()
 	is_controller_focused = false
 
 
 func _on_finish_next_level_button_gui_input(event):
-	if $NextLayer/FinishNextLevelButton.has_focus() and event is InputEventKey or event is InputEventJoypadButton:
+	if $FinishNextLevelButton.has_focus() and event is InputEventKey or event is InputEventJoypadButton:
 		is_controller_focused = true
 		if event.is_action_pressed("ui_left"):
 			accept_event() # prevent the normal focus-stuff from happening
-			$MenuLayer/FinishMenuButton.grab_focus()
+			$FinishMenuButton.grab_focus()
 		elif event.is_action_pressed("ui_up"):
 			accept_event()
-			$MenuLayer/FinishMenuButton.grab_focus()
+			$FinishMenuButton.grab_focus()
 		elif event.is_action_pressed("ui_down") or event.is_action_pressed("ui_right"):
 			accept_event() # prevent the normal focus-stuff from happening
-			$MenuLayer/FinishMenuButton.grab_focus()
+			$FinishMenuButton.grab_focus()
 
 
 func _on_finish_menu_button_gui_input(event):
-	is_controller_focused = true
-	if $MenuLayer/FinishMenuButton.has_focus() and event is InputEventKey or event is InputEventJoypadButton:
+	if $FinishMenuButton.has_focus() and event is InputEventKey or event is InputEventJoypadButton:
+		is_controller_focused = true
 		if event.is_action_pressed("ui_left"):
 			accept_event() # prevent the normal focus-stuff from happening
-			$NextLayer/FinishNextLevelButton.grab_focus()
+			$FinishNextLevelButton.grab_focus()
 		elif event.is_action_pressed("ui_up"):
 			accept_event()
-			$NextLayer/FinishNextLevelButton.grab_focus()
+			$FinishNextLevelButton.grab_focus()
 		elif event.is_action_pressed("ui_down") or event.is_action_pressed("ui_right"):
 			accept_event() # prevent the normal focus-stuff from happening
-			$NextLayer/FinishNextLevelButton.grab_focus() 
-
-
-func _on_finish_next_level_button_focus_exited():
-	$NextLayer/FinishNextLevelButton.release_focus()
-	is_controller_focused = false
-
-
-func _on_finish_menu_button_focus_exited():
-	$MenuLayer/FinishMenuButton.release_focus()
-	is_controller_focused = false
+			$FinishNextLevelButton.grab_focus()
--- Scripts/level1.gd
+++ Scripts/level1.gd
@@ -114,10 +114,10 @@ func _on_finish_player_entered():
 	$Player.get_child(1).animation = "stay"
 	$Player.set_physics_process(false)
 	await get_tree().create_timer(1).timeout
+	$Finish/FinishUI.is_UI_showing = true
+	$Finish/FinishUI.get_child(0).show()
 	$Finish/FinishUI.get_child(1).show()
 	$Finish/FinishUI.get_child(2).show()
-	$Finish/FinishUI.get_child(3).show()
-	$Finish/FinishUI.is_UI_showing = true
 
 
 func _on_start_area_player_exited_start_area():
