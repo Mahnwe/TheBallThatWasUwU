920910185e045b564ef5b8bd6d7acaf1d413dfd5
Manwe <mathieu.jourdan.zola@gmail.com>
2025-02-05 17:05:34 +0100

    Import and create new animations

--- Scripts/kelp.gd
+++ Scripts/kelp.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+@export
+var is_flip = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if is_flip:
+		$Sprite2D.flip_h = true
 	$Sprite2D.self_modulate.a = 0.7
+	$Sprite2D.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
