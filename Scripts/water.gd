b945db6ada30942a19d9edc1b6ab2be9f388bc6f
Manwe <mathieu.jourdan.zola@gmail.com>
2025-02-11 16:32:16 +0100

    Import and setup new animation for water

--- Scripts/water.gd
+++ Scripts/water.gd
@@ -1,9 +1,8 @@
-extends Sprite2D
-
+extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	self.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
