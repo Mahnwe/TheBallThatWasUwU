2c8d907a6069395ada344153e91fca811cb3b87c
Manwe <mathieu.jourdan.zola@gmail.com>
2025-03-18 15:44:43 +0100

    import new sprites and create scenes (tubes, metalPlatform, bird), add bird in level 1

---
+++ Scripts/bird.gd
@@ -0,0 +1,18 @@
+extends AnimatedSprite2D
+
+var parent_name
+@export var speed = 150
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	parent_name = get_parent()
+	self.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	_patrol(delta)
+	
+	
+func _patrol(delta):
+	if parent_name.get_name() == "PathFollow2D":
+		parent_name.progress += speed * delta
---
+++ Scripts/tube.gd
@@ -0,0 +1,9 @@
+extends AnimatedSprite2D
+
+func _ready():
+	self.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
