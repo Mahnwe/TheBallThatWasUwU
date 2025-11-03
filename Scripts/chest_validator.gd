a5192c191865f2d1e869926cd095f1096f4a3723
Manwe <mathieu.jourdan.zola@gmail.com>
2025-11-03 02:39:44 +0100

    Work on backgrounds, fix save bug with chests, finish to implement new save system and clean now unused directory and ranks

--- Scripts/chest_validator.gd
+++ Scripts/chest_validator.gd
@@ -3,10 +3,6 @@ extends Sprite2D
 var is_chest_valid
 var check_chest
 
-var properties_config = ConfigFile.new()
-# Load data from a file.
-var properties_file = properties_config.load("res://Ressources/PropertieFile/properties.cfg")
-
 func _ready():
 	is_chest_valid = false
 	check_chests()
@@ -16,26 +12,26 @@ func _process(_delta):
 	
 func check_chests():
 	if (self.get_parent().name == "Level1Button"):
-		check_chest = properties_config.get_value("Chests", "level_one_chest")
+		check_chest = $SaveManager.get_properties_value("Chests", "level_one_chest")
 		if check_chest:
 			is_chest_valid = true
 	if (self.get_parent().name == "Level2Button"):
-		check_chest = properties_config.get_value("Chests", "level_two_chest")
+		check_chest = $SaveManager.get_properties_value("Chests", "level_two_chest")
 		if check_chest:
 			is_chest_valid = true
 	if (self.get_parent().name == "Level3Button"):
-		check_chest = properties_config.get_value("Chests", "level_three_chest")
+		check_chest = $SaveManager.get_properties_value("Chests", "level_three_chest")
 		if check_chest:
 			is_chest_valid = true
 	if (self.get_parent().name == "Level4Button"):
-		check_chest = properties_config.get_value("Chests", "level_four_chest")
+		check_chest = $SaveManager.get_properties_value("Chests", "level_four_chest")
 		if check_chest:
 			is_chest_valid = true
 	if (self.get_parent().name == "Level5Button"):
-		check_chest = properties_config.get_value("Chests", "level_five_chest")
+		check_chest = $SaveManager.get_properties_value("Chests", "level_five_chest")
 		if check_chest:
 			is_chest_valid = true
 	if (self.get_parent().name == "Level6Button"):
-		check_chest = properties_config.get_value("Chests", "level_six_chest")
+		check_chest = $SaveManager.get_properties_value("Chests", "level_six_chest")
 		if check_chest:
 			is_chest_valid = true
